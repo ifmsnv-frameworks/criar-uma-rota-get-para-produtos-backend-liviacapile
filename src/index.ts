c949152e8d3824f3e87866e786b49dd6c4f4e57e
liviacapile <livia.antoniassi@estudante.ifms.edu.br>
2025-09-08 10:13:14 -0400

    Tarefa Livia Capile

--- src/index.ts
+++ src/index.ts
@@ -65,3 +65,27 @@ Faz pelo menos 3 inserções nessa tabela
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
+
+
+//Tarefa
+
+app.get('/produtos', async (req: Request, res: Response) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DBHOST!,
+            user: process.env.DBUSER!,
+            password: process.env.DBPASSWORD!,
+            database: process.env.DBNAME!,
+            port: Number(process.env.DBPORT!)
+        });
+        const [rows] = await connection.query('SELECT * FROM produtos');
+        await connection.end();
+        res.json(rows);
+    } catch (error: any) {
+        res.status(500).json({ erro: 'Erro ao buscar produtos', detalhe: error.message || error });
+    }
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
